df0f9e68371f901b53fb4c79725a4e759acb43be
luiz-surian-claro <luiz.filho.terceiros@claro.com.br>
2025-10-06 16:02:35 -0300

    Update Virtual environment script

--- run_app.ps1
+++ run_app.ps1
@@ -1,7 +1,28 @@
-python -m venv venv
-.\venv\Scripts\Activate.ps1
+# Set the venv folder name
+$venvFolder = "steganography-venv"
+
+# Set the script file name
+$scriptFile = "steganography.py"
+
+# Check if venv folder doesn't exist
+if (-Not (Test-Path -Path $venvFolder)) {
+    Write-Host "Creating virtual environment..."
+    python -m venv $venvFolder
+}
+
+# Activate the virtual environment
+& "$venvFolder\\Scripts\\Activate.ps1"
+
+# Upgrade pip and install dependencies
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-python .\steganography.py
+
+# Clear the console
+Clear-Host
+
+# Run the script
+python $scriptFile
+
+# Deactivate the virtual environment
 deactivate
 exit
